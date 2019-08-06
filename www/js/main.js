f065b6d86c2ac1e4ab67cfa9d6edcb39a6e2b885
KanadeNishizawa <kanade27@icloud.com>
2019-08-06 21:37:56 +0900

    色相修正、塗りを抜きに変更

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,4 @@
+
 let stage;
 let nameTag;
 let shapes;
@@ -8,11 +9,8 @@ let init = () => {
     nameTag = document.getElementById("nameTag");
 
     setup();
-
     createjs.Ticker.framerate = 30;
-    //毎フレーム実行される関数を指定
     createjs.Ticker.addEventListener("tick", loop);
-    //stage を自動的に更新する
     createjs.Ticker.addEventListener("tick", stage);
     if (createjs.Touch.isSupported()) {
         createjs.Touch.enable(stage);
@@ -20,19 +18,33 @@ let init = () => {
 }
 
 
+
+
 let setup = () => {
     nameTag.textContent = "201812121 Nishizawa Kanade";
     shapes = new Array();
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 30; i++) {
 
         let s = new createjs.Shape();
-        let color1 = Math.random() * 360;
+        let angle = 0;
+        let radius = 15;
+        let speed = 0.01;
+        let centerHue = 180;
 
-        s.graphics.beginFill("hsl(" + color1 + ", 100%, 50%)");
-        s.graphics.drawRect(-20, -20, 40, 40);
+        createjs.Ticker.addEventListener("tick", handleTick);
+        function handleTick() {
+            let hue = centerHue + Math.cos(angle) * radius;
+            let color = "hsl(" + hue + ", 100%, 50%)";
+
+            s.graphics.beginStroke(color);
+            s.graphics.setStrokeStyle(20);
+            s.graphics.drawRect(-20, -20, 200, 200);
+            angle += speed;
+        }
+
         s.x = 150;
         s.y = 150;
-        s.alpha = 0.8;
+        s.alpha = 0.7;
         s.vx = 10 * Math.random() - 5;
         s.vy = 10 * Math.random() - 5;
 
@@ -43,6 +55,8 @@ let setup = () => {
 
 }
 
+
+
 let loop = () => {
     let time = new Date();
     let sec = time.getSeconds();
@@ -52,18 +66,12 @@ let loop = () => {
         oldSeconds = sec;
         update = true;
     }
-
     //shapes[0]を円軌道で動かす
     // let shape0 = shapes[0];
     // shape0.x = Math.cos((r - 90) * (Math.PI / 180)) * 130 + 150;
     // shape0.y = Math.sin((r - 90) * (Math.PI / 180)) * 130 + 150;
-
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 30; i++) {
         let s = shapes[i];
-        function rotate() {
-            s.rotation += .5;
-        }
-
 
         if (s.x >= 300 || s.x <= 0) {
             s.vx *= -1;
