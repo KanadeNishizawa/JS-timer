8bda2846cefb40cbbe0ea6ebe41d7dbadada173c
KanadeNishizawa <kanade27@icloud.com>
2019-08-08 03:46:05 +0900

    色変更

--- www/js/main.js
+++ www/js/main.js
@@ -18,40 +18,52 @@ let init = () => {
 
 let setup = () => {
   nameTag.textContent = "201812121 Nishizawa Kanade";
-  shapes = new Array();
-
-  let bg = new createjs.Shape();
-  bg.graphics
-    .beginFill("black")
-    .drawRect(0, 0, 300, 300)
-    .beginLinearGradientFill(["#9ef", "#9e2"], [0.1, 1.0], 0, -100, 50, 100);
-  stage.addChild(bg);
-
-  for (let i = 0; i < 200; i++) {
-    let s = new createjs.Shape();
+ 
+    let bg = new createjs.Shape();
     let angle = 0;
     let radius = 50;
     let speed = 0.01;
     let centerHue = 180;
+    let centerHue2 = 90;
 
-    createjs.Ticker.addEventListener("tick", handleTick);
-    function handleTick() {
+  createjs.Ticker.addEventListener("tick", handleTick);
+  function handleTick() {
       let hue = centerHue + Math.cos(angle) * radius;
-      let color = "hsl(" + hue + ", 90%, 50%)";
-      s.graphics.beginStroke(color);
+      let hue2 = centerHue2 + Math.cos(angle) * radius;
+      let color1 = "hsl(" + hue + ", 60%, 30%)";
+      let color2 = "hsl(" + hue2 + ", 60%, 30%)";      
+      bg.graphics
+      .beginRadialGradientFill(
+        [color1, color2],
+        [0.01, 1.0],
+        150,
+        150,
+        1,
+        150,
+        150,
+        200
+      )
+      .drawRect(0, 0, 300, 300);
+    angle += speed;
+    stage.addChild(bg);
+  }
+    
+    shapes = new Array();
+
+  for (let i = 0; i < 200; i++) {
+    let s = new createjs.Shape();
+      s.graphics.beginStroke("#ffffff");
       s.graphics.setStrokeStyle(0.1);
       s.graphics.drawRect(0, 0, 20, 16);
-      angle += speed;
-    }
-    s.alpha = 0;
-    s.x = 160;
-    s.y = 160;
-    s.regX = 10;
-    s.regY = 7;
-
-    stage.addChild(s);
-    shapes.push(s);
+      s.alpha = 0;
+      s.x = 160;
+      s.y = 160;
+      s.regX = 10;
+      s.regY = 7;
+      stage.addChild(s);
+      shapes.push(s);
   }
+    
 };
 
 let loop = () => {
@@ -71,11 +83,12 @@ let loop = () => {
       .to(
         { rotation: 140, scaleX: 32, scaleY: 30, alpha: 0.1 },
         3500,
-          createjs.Ease.quadIn)
-        .call(remove);
-      
-      function remove() {
-         stage.removeChild(s)
-      }
+        createjs.Ease.quadIn
+      )
+      .call(remove);
+
+    function remove() {
+      stage.removeChild(s);
+    }
   }
 };
