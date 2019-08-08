c5a9ae9be7a02fd1ca09008e81b40c780e74a0b0
KanadeNishizawa <kanade27@icloud.com>
2019-08-09 07:28:33 +0900

    秒数追加

--- www/js/main.js
+++ www/js/main.js
@@ -141,10 +141,256 @@ let setup = () => {
     stage.setChildIndex(d, 1);
   }
 
-  let time = new createjs.Text("Hello World", "bold 30px sans-serif", "white");
-  stage.addChild(time);
-  time.x = 80;
-  time.y = 135;
+  let time1 = new createjs.Text("1", "1px sans-serif", "white");
+  time1.x = 145;
+  time1.y = 140;
+  stage.addChild(time1);
+  stage.setChildIndex(time1, 0);
+
+  createjs.Tween.get(time1)
+    .wait(200)
+    .to(
+      { rotation: -30, scaleX: 150, scaleY: 150, alpha: 0 },
+      2200,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time1);
+  }
+  let time8 = new createjs.Text("2", "1px sans-serif", "white");
+  time8.x = 155;
+  time8.y = 160;
+  time8.regX = 2;
+  time8.regY = 2;
+  stage.addChild(time8);
+  stage.setChildIndex(time8, 0);
+
+  createjs.Tween.get(time8)
+    .wait(1000)
+    .to(
+      { rotation: 50, scaleX: 150, scaleY: 100, alpha: 0 },
+      2200,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time8);
+  }
+
+  let time2 = new createjs.Text("3", "1px sans-serif", "white");
+  time2.x = 145;
+  time2.y = 140;
+  stage.addChild(time2);
+  stage.setChildIndex(time2, 0);
+
+  createjs.Tween.get(time2)
+    .wait(2000)
+    .to(
+      { rotation: 70, scaleX: 270, scaleY: 220, alpha: 0 },
+      2400,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time2);
+  }
+
+  let time9 = new createjs.Text("4", "1px sans-serif", "white");
+  time9.x = 175;
+  time9.y = 180;
+  time9.regX = 2;
+  time9.regY = 2;
+  stage.addChild(time9);
+  stage.setChildIndex(time9, 0);
+
+  createjs.Tween.get(time9)
+    .wait(3000)
+    .to(
+      { rotation: 180, scaleX: 220, scaleY: 180, alpha: 0 },
+      2600,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time9);
+  }
+
+  let time3 = new createjs.Text("5", "1px sans-serif", "white");
+  time3.x = 155;
+  time3.y = 160;
+  stage.addChild(time3);
+  stage.setChildIndex(time3, 0);
+
+  createjs.Tween.get(time3)
+    .wait(3900)
+    .to(
+      { rotation: 30, scaleX: 270, scaleY: 220, alpha: 0 },
+      2000,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time3);
+  }
+
+  let time10 = new createjs.Text("6", "1px sans-serif", "white");
+  time10.x = 155;
+  time10.y = 160;
+  stage.addChild(time10);
+  stage.setChildIndex(time10, 0);
+
+  createjs.Tween.get(time10)
+    .wait(5000)
+    .to(
+      { rotation: 190, scaleX: 180, scaleY: 180, alpha: 0 },
+      2000,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time10);
+  }
+
+  let time4 = new createjs.Text("7", "1px sans-serif", "white");
+  time4.x = 145;
+  time4.y = 140;
+  stage.addChild(time4);
+  stage.setChildIndex(time4, 0);
+
+  createjs.Tween.get(time4)
+    .wait(6000)
+    .to(
+      { rotation: -35, scaleX: 150, scaleY: 150, alpha: 0 },
+      2200,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time4);
+  }
+
+  let time11 = new createjs.Text("8", "1px sans-serif", "white");
+  time11.x = 155;
+  time11.y = 160;
+  stage.addChild(time11);
+  stage.setChildIndex(time11, 0);
+
+  createjs.Tween.get(time11)
+    .wait(7000)
+    .to(
+      { rotation: 250, scaleX: 270, scaleY: 220, alpha: 0 },
+      2000,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time11);
+  }
+
+  let time5 = new createjs.Text("9", "1px sans-serif", "white");
+  time5.x = 175;
+  time5.y = 170;
+  time5.regX = 2;
+  time5.regY = 2;
+  stage.addChild(time5);
+  stage.setChildIndex(time5, 0);
+
+  createjs.Tween.get(time5)
+    .wait(8000)
+    .to(
+      { rotation: -170, scaleX: 230, scaleY: 300, alpha: 0 },
+      2600,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time5);
+  }
+
+  let time12 = new createjs.Text("10", "1px sans-serif", "white");
+  time12.x = 145;
+  time12.y = 140;
+  stage.addChild(time12);
+  stage.setChildIndex(time12, 0);
+
+  createjs.Tween.get(time12)
+    .wait(9000)
+    .to(
+      { rotation: -20, scaleX: 180, scaleY: 170, alpha: 0 },
+      2200,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time12);
+  }
+
+  let time6 = new createjs.Text("11", "1px sans-serif", "white");
+  time6.x = 135;
+  time6.y = 180;
+  stage.addChild(time6);
+  stage.setChildIndex(time6, 0);
+
+  createjs.Tween.get(time6)
+    .wait(10000)
+    .to(
+      { rotation: -150, scaleX: 220, scaleY: 220, alpha: 0 },
+      2600,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time6);
+  }
+
+  let time7 = new createjs.Text("12", "1px sans-serif", "white");
+  time7.x = 125;
+  time7.y = 130;
+  stage.addChild(time7);
+  stage.setChildIndex(time7, 0);
+
+  createjs.Tween.get(time7)
+    .wait(11000)
+    .to(
+      { rotation: 60, scaleX: 350, scaleY: 300, alpha: 0 },
+      2600,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time7);
+  }
+  let time13 = new createjs.Text("12", "1px sans-serif", "white");
+  time13.x = 155;
+  time13.y = 160;
+  stage.addChild(time13);
+  stage.setChildIndex(time13, 0);
+
+  createjs.Tween.get(time13)
+    .wait(11001)
+    .to(
+      { rotation: 30, scaleX: 300, scaleY: 300, alpha: 0 },
+      2600,
+      createjs.Ease.quadIn
+    )
+    .call(remove);
+
+  function remove() {
+    stage.removeChild(time13);
+  }
 };
 
 let loop = () => {
